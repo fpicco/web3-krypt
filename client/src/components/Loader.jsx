a99a1d03d20e3bad8e6d365968d6acc071f6f354
fpicco <flor.picco@gmail.com>
2022-01-02 08:13:35 -0300

    Add welcome section's layout

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,5 +1,8 @@
 const Loader = () => {
-    return <h1>Loader</h1>;
-  };
-  export default Loader;
-  
+  return (
+    <div className="flex justify-center items-center py-2">
+      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-red-700"></div>
+    </div>
+  );
+};
+export default Loader;
